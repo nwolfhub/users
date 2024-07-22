6a0f608f92af6d3aabc0eaba3b97a149f615d857
s0m31 <admin@nwolfhub.org>
2024-07-22 13:19:01 +0300

    Init project

---
+++ src/test/java/org/nwolfhub/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nwolfhub.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
